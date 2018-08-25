2666d160b78854f10809592f7bd95c35419fe78a
Estela Medrano Jiménez <stlmedrano@gmail.com>
2018-08-25 21:38:23 +0200

    google strategy to connect and get credentials

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
-var express = require('express');
-var app = express();
-var exports = module.exports = {};
+const express = require('express')
 
-app.get('/', function(req, res){
-  res.send('Hello World');
-});
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
+const app = express();
+
+passport.use(
+ 
+  new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSerect,
+  callbackURL: '/auth/google/callback'
+  }, (accesToken, resfreshToken, profile, done) =>{
+    //user information
+      console.log('accesToken',accesToken);
+      console.log('profile',profile);
+      console.log('done',accesToken);
+
+}));
+//forward users's request to google
+app.get(
+    '/auth/google',
+    passport.authenticate('google',{
+      scope: ['profile','email']
+    })
+);
+
+//user grant permission, take the code from the url
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google',{
+    scope: ['profile','email']
+  })
+);
+
+
 
 const PORT = process.env.PORT || 5000;
-var server = app.listen(PORT, function(){
-  console.log('Magic is happening on port 5000');
-});
+var server = app.listen(PORT);
 
 exports.closeServer = function(){
   server.close();
