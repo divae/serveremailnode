04bea1e03a6a7099bebc3a3c80e3983d24dd615d
Estela Medrano Jiménez <stlmedrano@gmail.com>
2018-08-25 10:19:57 +0200

    add url repository in hi

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', ( req, res) => {
-    res.send({ hello: 'world' });
+    res.send({ 
+        hello: 'world', 
+        repository:'https://github.com/divae/serveremailnode'
+    });
 });
 
 const PORT = process.env.PORT || 5000;
