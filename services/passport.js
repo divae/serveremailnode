2b1a946ee72e0ab8516dccc60fb930a09f61bd07
Estela Medrano Jiménez <stlmedrano@gmail.com>
2018-08-26 09:57:47 +0200

    finish wiring up the authentication flow

--- services/passport.js
+++ services/passport.js
@@ -5,14 +5,32 @@ const keys = require('../config/keys');
 //get collection users (1 arg)
 const User = mongoose.model('users');
 
+passport.serializeUser((user,done) => {
+
+});
+
+passport.deserializeUser((id,done) => {
+    user.findById(id).then( user =>{
+        done(null,user);
+    });
+        
+});
+
 passport.use( 
     new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSerect,
     callbackURL: '/auth/google/callback'
     }, (accesToken, resfreshToken, profile, done) =>{
-      new User({
-          googleId: profile.id
-      }).save();
+       
+        User.findOne({googleID:profile.id}).then((exisingUser) => {
+            if(exisingUser){
+                done(null, exisingUser);
+            }else{
+                new User({googleId: profile.id})
+                .save()
+                .then(user => done(null, user));
+            }
+        });     
   
 }));
