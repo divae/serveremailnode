15496a598271a7687ee1c6220017fb8605b79076
Estela Medrano Jiménez <stlmedrano@gmail.com>
2018-08-26 22:43:22 +0200

    inicialize and generate sesion

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
+const cookieSession = require('cookie-session');
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
+
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(cookieSession({
+  name: 'session',
+  keys: [keys.cookieKey],
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
