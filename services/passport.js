c77a64c8858088285f3ce43392c082245f35f4f5
Estela Medrano Jiménez <stlmedrano@gmail.com>
2018-08-26 22:18:55 +0200

    if user exist, not save

--- services/passport.js
+++ services/passport.js
@@ -6,7 +6,7 @@ const keys = require('../config/keys');
 const User = mongoose.model('users');
 
 passport.serializeUser((user,done) => {
-
+ 
 });
 
 passport.deserializeUser((id,done) => {
@@ -22,7 +22,18 @@ passport.use(
       clientSecret: keys.googleClientSerect,
       callbackURL: '/auth/google/callback'
     }, (accesToken, resfreshToken, profile, done) =>{
+        User.findOne({googleId: profile.id})
+        .then((existingUser)=>{
+            if(existingUser){
+                //we already have a record with the given profile ID
+                done(null,existingUser);
+            }else{
+                //we don't have a user record with this ID, make a new record
+                new User({googleId:profile.id})
+                .save()
+                .then( user => done(null,user));
+            }
+        })
       //user information
-      new User({googleId:profile.id}).save();      
 
 }));
