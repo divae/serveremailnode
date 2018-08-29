7e6a5fc3b4e6fa8faae5a35d4f4e49960eaeb88a
Estela Medrano Jiménez <stlmedrano@gmail.com>
2018-08-29 14:11:04 +0200

    refractor app to class

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,7 +3,8 @@ import { FETCH_USER } from 'action/types';
 
 const fechUser = () => {
     return function (dispatch) {
-        axios.get('/api/current_user')
+        axios.
+        get('/api/current_user')
         .then(res => dispatch({type : FETCH_USER, payload : res})
         );
 
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-
 import Header from 'components/Header';
 
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
 
-const App = () =>{
-    return(
-        <div className="container">
-            <BrowserRouter>
-                <div>
-                    <Header />
-                    <Route exact path="/" component={Landing} />
-                    <Route exact path="/surveys" component={Dashboard} />
-                    <Route path="/surveys/news" component={SurveyNew} />
-                </div>
-            </BrowserRouter>
-        </div>
-    );
+class  extends Component {
+    render() { 
+        return(
+            <div className="container">
+                <BrowserRouter>
+                    <div>
+                        <Header />
+                        <Route exact path="/" component={Landing} />
+                        <Route exact path="/surveys" component={Dashboard} />
+                        <Route path="/surveys/news" component={SurveyNew} />
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    }
 }
  
+
 export default App;
