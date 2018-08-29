2c0b04d6cc333d4d5d73d42a166fd667f6250665
Estela Medrano Jiménez <stlmedrano@gmail.com>
2018-08-29 17:05:56 +0200

    refractor 2

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,7 +2,4 @@ import axios from 'axios';
 import { FETCH_USER } from 'actions/types';
 
 
-export const fechUser = () => async dispatch => {
-    const res = await axios.get('/api/current_user');
-    dispatch({ type : FETCH_USER,  payload : res });
-};
+export const fechUser = () => async dispatch => dispatch({ type : FETCH_USER,  payload : await axios.get('/api/current_user') });
