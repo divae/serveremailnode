2e509ca8cee3567dbd5498f207344249cf3878c1
Estela Medrano Jiménez <stlmedrano@gmail.com>
2018-08-29 14:05:40 +0200

    whenever the action creators called it will return a function see the return and it will aotoactically call  it with the dispatch

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,5 +2,10 @@ import axios from 'axios';
 import { FETCH_USER } from 'action/types';
 
 const fechUser = () => {
-    axios.get('/api/current_user');
+    return function (dispatch) {
+        axios.get('/api/current_user')
+        .then(res => dispatch({type : FETCH_USER, payload : res})
+        );
+
+    }
 }
