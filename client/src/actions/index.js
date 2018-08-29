a8637bd00dcda4f0e8972b653b6874cde2a39a10
Estela Medrano Jiménez <stlmedrano@gmail.com>
2018-08-29 14:40:06 +0200

    clean code

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
-import { FETCH_USER } from 'action/types';
-
+import { FETCH_USER } from 'actions/types';
 const fechUser = () => {
     return function (dispatch) {
-        axios.
-        get('/api/current_user')
+        axios
+        .get('/api/current_user')
         .then(res => dispatch({type : FETCH_USER, payload : res})
         );
 
     }
-}
+};
