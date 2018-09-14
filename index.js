b9c858968bb40ba054c4d812690c7417a5ffc843
Estela Medrano Jiménez <stlmedrano@gmail.com>
2018-09-15 00:06:23 +0200

    add billing routes

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
-
+require('./routes/billingRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
   //express vill serve up production assets
