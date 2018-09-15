2fa2cb4ff5e2ec501d69256cbd12271e4827ca15
Estela Medrano Jiménez <stlmedrano@gmail.com>
2018-09-16 01:21:36 +0200

    generate route /aoi/surveys

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
   //express vill serve up production assets
