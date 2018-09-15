ada66993d14642798c935ee3a623cf355d4e5343
Estela Medrano Jiménez <stlmedrano@gmail.com>
2018-09-15 18:36:36 +0200

    refractor

--- config/prod.js
+++ config/prod.js
@@ -5,7 +5,7 @@ module.exports = {
     googleClientSerect : process.env.GOOGLE_CLIENT_SECRET ,
     mongoURI: process.env.MONGO_URI ,
     cookieKey: process.env.COOKIE_KEY ,
-    stripePublishablekey:process.env.STRIPE_PUBLISHABLE_KEY,
-    stripesSecretkey:process.env.STRIPE_SECRET_KEY
+    stripePublishableKey:process.env.STRIPE_PUBLISHABLE_KEY,
+    stripeSecretKey:process.env.STRIPE_SECRET_KEY
 }
 
