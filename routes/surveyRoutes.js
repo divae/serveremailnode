a7a485ffc06254c4af9092fe1e61a1c553b1e741
Estela Medrano Jiménez <stlmedrano@gmail.com>
2018-09-16 01:24:54 +0200

    require login in api/surveys

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,5 +1,7 @@
+const requireLogin = require('../middkewares/requireLogin');
+
 module.exports = app => {
-    app.post('/api/surveys', (req,res) => {
+    app.post('/api/surveys', requireLogin, (req,res) => {
 
     });
 };
