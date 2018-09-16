46fb6565c9747a34bc09dc2eb04c6eca5d590090
Estela Medrano Jiménez <stlmedrano@gmail.com>
2018-09-16 17:52:55 +0200

    add require class Survey

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -2,13 +2,14 @@ const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 const Mailer = require('../services/Mailer');
+const Survey = mongoose.model('surveys');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 module.exports = app => {
     app.post('/api/surveys', requireLogin,requireCredits, (req,res) => {
         const { title, subject, body , recipients } = req.body;
 
-        const survey = new survey({
+        const survey = new Survey({
             title,
             subject,
             body,
@@ -18,5 +19,7 @@ module.exports = app => {
         });
 
         const mailer = new Mailer(survey, surveyTemplate(survey));
+
+        mailer.send();
     });
 };
