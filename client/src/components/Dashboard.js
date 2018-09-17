5896816defe91aa045054be7404db1beddff967c
Estela Medrano Jiménez <stlmedrano@gmail.com>
2018-09-18 00:45:55 +0200

    add button to link survey form

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import SurveyNew from 'components/surveys/SurveyNew';
 
 const Dashboard = () => {
     return(
         <div>
-            <SurveyNew />
+            <div className="fixed-action-btn">
+                <Link to="/surveys/news" className="btn-floating btn-large red">
+                    <i className="material-icons">add</i>
+                </Link>              
+            </div>
         </div>
     );
 };
