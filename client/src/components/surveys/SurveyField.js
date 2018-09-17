b76db9f28aebf819d029d425ef2e5e28f5ca2d71
Estela Medrano Jiménez <stlmedrano@gmail.com>
2018-09-18 01:04:10 +0200

    validate form

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -2,11 +2,14 @@
 //label and text input
 import React from 'react';
 
-export default ({input,label}) => {
+export default ({input,label, meta:{error,touched}}) => {
     return(
         <div>
             <label>{label}</label>
             <input {...input} />
+            <div className="red-text">
+                {touched && error}
+            </div>
         </div>
     );
 };
