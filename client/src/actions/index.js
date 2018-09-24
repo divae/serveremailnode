2af0e0faf90f316145c1123a759efdbe21ae70a1
Estela Medrano Jiménez <stlmedrano@gmail.com>
2018-09-24 21:41:45 +0200

    prepared surveylist

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -22,7 +22,7 @@ export const submitSurvey = (values,history) => async dispatch => {
     dispatch({ type : FETCH_USER, payload : res.data });
 }
 
-export const fechSuerveys = () => async dispatch => {
+export const fetchSurveys = () => async dispatch => {
     const res = await axios.get('/api/surveys');
 
     dispatch({type:FETCH_SURVEYS, payload:res.data});
