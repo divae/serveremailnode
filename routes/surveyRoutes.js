cfa0bc014ee711e7e2c4cbd801f14bc3e0a3f2d5
Estela Medrano Jiménez <stlmedrano@gmail.com>
2018-09-25 11:51:52 +0200

    change choise to choice

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -19,7 +19,7 @@ module.exports = app => {
         res.send(surveys);
     });
 
-    app.get('/api/surveys/:surveyId/:choise', (req,res) => {
+    app.get('/api/surveys/:surveyId/:choice', (req,res) => {
         res.send('Thanks for voting!');
     });
 
